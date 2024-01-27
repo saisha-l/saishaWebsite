13c80ae61bfafc16c1ded12a28c25e2ceb645266
Saisha <87923617+saisha-l@users.noreply.github.com>
2024-01-27 12:00:57 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+
+
+function App() {
+  return (
+
+  
+    <div className="App" >
+      <div class="headir">
+
+      <img class="me" src="https://i.pinimg.com/736x/0e/ed/53/0eed53776d91886bc06e72e508f5cfc1.jpg" alt="Logo" />
+  
+    <h2 class="name" > saisha lakkoju. </h2> 
+    <a href="https://docs.google.com/document/d/1JHNUgk4xTAFX0SBYu961bek3nUDlQioW1MP2VCIO1EQ/edit?usp=sharing">
+      
+ 
+ <text class="button" >
+   
+resume
+</text>
+
+
+
+
+    </a>
+    <a href="https://docs.google.com/document/d/1JHNUgk4xTAFX0SBYu961bek3nUDlQioW1MP2VCIO1EQ/edit?usp=sharing">
+      <text class="button" >
+     hi
+     </text>
+    </a>
+   
+
+
+</div>
+    <h1>
+
+    </h1>
+
+  
+
+
+    </div>
+  );
+}
+
+export default App;
